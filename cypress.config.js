445cf93d90a18b1dbe55c6ff07e6aca99bfd11e5
Srabani <srabaniupadhyaya@yahoo.com>
2024-08-25 18:46:15 +0100

    initial spec

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const getCompareSnapshotsPlugin = require('cypress-image-diff-js/plugin');
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return getCompareSnapshotsPlugin(on, config);
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import './commands'
+const compareSnapshotCommand = require('cypress-image-diff-js/command');
+ 
+
+
+compareSnapshotCommand();
